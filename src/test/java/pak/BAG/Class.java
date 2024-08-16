b8c4e16761d2a183a99693d3ac749544a1965246
ManojKaarthick <manojkaarthick2@gmail.com>
2024-08-16 21:53:05 +0530

    kkkkkkkk

--- src/test/java/pak/BAG/Class.java
+++ src/test/java/pak/BAG/Class.java
@@ -3,7 +3,12 @@ package pak.BAG;
 public class Class {
 	
 	public void method1() {
+		System.out.println("good evening");
 		
+	}
+	
+	public void method2() {
+		System.out.println("good night");
 	}
 
 }
